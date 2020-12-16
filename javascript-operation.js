3b22fabf0e85a2ec03310dd45514bebfdef47222
semo4 <fotboy1788@hotmail.com>
2020-12-16 14:17:19 +0200

    add new javascript file

---
+++ javascript-operation.js
@@ -0,0 +1,81 @@
+
+
+var g = prompt("enter guess number ?");
+
+guess(g);
+
+function guess(g){
+    if(g >=  25 && g < 30){
+        alert('you almost there');
+
+    }
+    else if (g <= 50 && g > 30){
+        alert('you Far away for the correct answer');
+    }
+
+    else if(g == 30){
+        alert('you have the correct answer');
+    }
+}
+
+
+
+var con = confirm("let's play new game")
+var result ;
+if( con == true){
+     alert("you  wont to paly");
+    game();
+} else{
+    alert( "you don't wont to paly");
+}
+
+
+function game(){
+    var count = 0;
+    alert("remmber the number 100254");
+    result = prompt("how meny number you see?");
+    var convert = parseInt(result);
+    if (convert == 6){
+        count +=1;
+        alert('thats right it is the correct number');
+    }else{
+        alert('thats not right it is not the correct number');
+    }
+
+    result = prompt("if you add the last to  number to each other what is the result ?");
+    var number = parseInt(result);
+    if (number == 9){
+        count +=1;
+        alert('thats right it is the correct number');
+    }else{
+        alert('thats not right it is not the correct number');
+
+    }
+
+    result = prompt("if you add 1000 to the number?");
+    var add = parseInt(result);
+    if (add == 101254){
+        count +=1;
+        alert('thats right it is the correct number');
+    }else{
+        alert('thats not right it is not the correct number');
+
+    }
+
+    if (count == 3){
+        alert("you an awseme person thank you ");
+    }
+    else{
+        var check =  confirm("you have some question with wrong answer, do you wont to try again");
+        if( check == true){
+            alert("Let's start again");
+           game();
+       } else{
+           alert( "you don't wont to paly");
+       }
+    }
+    
+
+
+
+}
